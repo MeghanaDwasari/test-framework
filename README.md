20917bdf225973b792b4c7c23e419a90e7f69f2a
meghana <meghanadwasari@gmail.com>
2026-03-16 11:09:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+UI Automation Level 2 Framework
+
+Tech Stack
+- Selenium
+- Pytest
+- Docker Selenium Grid
+- Allure Reports
+
+Run Tests
+
+pytest -n 3
+
+Run with Allure
+
+pytest --alluredir=reports
+allure serve reports
---
+++ utils/driver_factory.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from config.settings import GRID_URL
+
+
+def create_driver():
+
+    options = Options()
+
+    driver = webdriver.Remote(
+        command_executor=GRID_URL,
+        options=options
+    )
+
+    driver.maximize_window()
+
+    return driver
